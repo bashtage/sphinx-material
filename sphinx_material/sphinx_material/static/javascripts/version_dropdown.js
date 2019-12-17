7c461869ae8032f8cd9598635a28776b6f0a76ca
thequackdaddy <pquack@gmail.com>
2019-12-16 21:27:14 -0600

    Refactor javascript.

--- sphinx_material/sphinx_material/static/javascripts/version_dropdown.js
+++ sphinx_material/sphinx_material/static/javascripts/version_dropdown.js
@@ -1,5 +1,5 @@
 function add_version_dropdown(json_loc, target_loc, text) {
-  $.getJSON(json_loc).done(function(versions) {
+
     var dropdown = document.createElement("div");
     dropdown.className = "md-flex__cell md-flex__cell--shrink dropdown";
     var button = document.createElement("button");
@@ -9,28 +9,24 @@ function add_version_dropdown(json_loc, target_loc, text) {
     content.className = "dropdown-content";
     dropdown.appendChild(button);
     dropdown.appendChild(content);
+
+    $.getJSON(json_loc).done(function(versions) {
+        for (var i = 0; i < versions.length; i++) {
+            if (versions[i].substring(0, 1) == "v") {
+                versions[i] = [versions[i], versions[i].substring(1)];
+            } else {
+                versions[i] = [versions[i], versions[i]];
+            };
+        };
+        for (var i = 0; i < versions.length; i++) {
+            var a = document.createElement("a");
+            a.innerHTML = versions[i][1];
+            a.href = target_loc + versions[i][0] + "/index.html";
+            a.title = versions[i][1];
+            content.appendChild(a);
+        };
+    }).fail(function() {
+        button.innerHTML = "Other Versions Not Found";
+    });
     $(".navheader").append(dropdown);
-    for (var i = 0; i < versions.length; i++) {
-      if (versions[i].substring(0, 1) == "v") {
-        versions[i] = [versions[i], versions[i].substring(1)];
-      } else {
-        versions[i] = [versions[i], versions[i]];
-      };
-    };
-    for (var i = 0; i < versions.length; i++) {
-      var a = document.createElement("a");
-      a.innerHTML = versions[i][1];
-      a.href = target_loc + versions[i][0] + "/index.html";
-      a.title = versions[i][1];
-      $(".dropdown-content").append(a);
-    };
-  }).fail(function() {
-    var dropdown = document.createElement("div");
-    dropdown.className = "md-flex__cell md-flex__cell--shrink dropdown";
-    var button = document.createElement("button");
-    button.className = "dropdownbutton";
-    button.innerHTML = "Other Versions Not Found";
-    dropdown.appendChild(button);
-    $(".navheader").append(dropdown);
-  });
 };
