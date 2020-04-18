0a389f3bd610b18976486d521e47007343b163ba
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-04-18 08:56:14 +0000

    Update docs after building Travis build 123 of bashtage/sphinx-material
    The docs were built from the branch 'master' against the commit
    e716623573c6a9a1f2d2c63db44d5bbe72d97b38.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/sphinx-material/jobs/676520462.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.6/bin/doctr deploy --build-tags --built-docs docs/_build/html/ devel

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.23+6.g4768f0c',
+    VERSION: '0.0.23+9.ge716623',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
